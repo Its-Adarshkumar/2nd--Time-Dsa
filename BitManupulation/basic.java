25b8b0a414c0a31ec853c182714852527e0cfc15
Its-Adarshkumar <adarshsinghrajput414@gmail.com>
2026-08-23 01:36:23 +0530

    Add TwoKiPower, basic, and dataTypes classes with bit manipulation examples

---
+++ BitManupulation/basic.java
@@ -0,0 +1,13 @@
+package BitManupulation;
+
+public class basic {
+    public static void main(String[] args) {
+        System.out.println(51&42);
+        System.out.println(51|42);
+
+        int x=5;
+        System.out.println(~x);// one's compliment
+        System.out.println(~x+1); // 2's compliment
+        System.out.println(-x);// 2's compliment
+    }
+}
---
+++ BitManupulation/dataTypes.java
@@ -0,0 +1,9 @@
+package BitManupulation;
+
+public class dataTypes {
+    public static void main(String[] args) {
+        byte x = 127;
+        x+=3;
+        System.out.println(x);
+    }
+}
